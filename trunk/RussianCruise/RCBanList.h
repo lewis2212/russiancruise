35f8f74fe31ca785466896d6360f048d7def4c34
turbosnail.rus <turbosnail.rus@e20eac7a-df01-d0be-1f0a-6011d65844bd>
2013-03-04 18:21:28 +0000

    Добавил бан команду. удалил лишние заголовки

---
+++ trunk/RussianCruise/RCBanList.h
@@ -0,0 +1,29 @@
+using namespace std;
+#ifndef _RC_BANLIST_H
+#define _RC_BANLIST_H
+
+#include "RCBaseClass.h"
+
+
+struct BanPlayer
+{
+	char    UName[24];             // Username
+    char    PName[24];             // Player name
+    byte    UCID;                  // Connection ID
+};
+
+class RCBanList:public RCBaseClass
+{
+	public:
+		RCBanList();
+		~RCBanList();
+		int init( const char* RootDir, void *CInSim );
+
+		void removeUser( const char* username );
+		void addUser( const char* username, time_t timestamp );
+
+	private:
+		void insim_ncn( struct IS_NCN* packet );
+};
+
+#endif // RCBANLIST_H
