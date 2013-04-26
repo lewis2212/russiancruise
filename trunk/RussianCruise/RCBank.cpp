6f439dc380960581219412fe7fc54947064ab0e9
Lexanom <Lexanom@e20eac7a-df01-d0be-1f0a-6011d65844bd>
2013-04-26 08:56:46 +0000

    - исправлена ошибка бесконечных кредитов - реализован вклад (не тестировал)

--- trunk/RussianCruise/RCBank.cpp
+++ trunk/RussianCruise/RCBank.cpp
@@ -21,7 +21,7 @@ int RCBank::GetCash(byte UCID)
 bool RCBank::AddCash(byte UCID, int Cash, bool Show = false)
 {
     char Text[96];
-    sprintf(Text,"^5| ^7^CНа ваш счет поступило: %d ^3RUR.",Cash);
+    sprintf(Text,"^5| ^7^CНа ваш счет поступило: %d ^3RUR^7.",Cash);
     if( Show )
         send_mtc(UCID, Text);
 
@@ -32,7 +32,7 @@ bool RCBank::AddCash(byte UCID, int Cash, bool Show = false)
 bool RCBank::RemCash(byte UCID, int Cash)
 {
     char Text[96];
-    sprintf(Text,"^5| ^7^CС вашего счета списано: %d ^3RUR.",Cash);
+    sprintf(Text,"^5| ^7^CС вашего счета списано: %d ^3RUR^7.",Cash);
     send_mtc(UCID, Text);
     players[ UCID ].Cash -= Cash;
     return true;
@@ -194,6 +194,44 @@ void RCBank::insim_ncn( struct IS_NCN* packet )
 
     mysql_free_result( rcbankRes );
 
+    /** вклады **/
+    sprintf(query,"SELECT cash, date_create FROM bank_deposits WHERE username='%s' LIMIT 1;",packet->UName);
+
+    if( mysql_ping( &rcbankDB ) != 0 )
+    {
+        printf("Error deposits: connection with MySQL server was lost\n");
+    }
+    if( mysql_query( &rcbankDB , query) != 0 )
+    {
+        printf("Error deposits: MySQL Query\n");
+    }
+
+    rcbankRes = mysql_store_result( &rcbankDB );
+    if(rcbankRes == NULL) printf("Error deposits: can't get the result description\n");
+
+    if(mysql_num_rows( rcbankRes ) > 0)
+    {
+        rcbankRow = mysql_fetch_row( rcbankRes );
+        players[ packet->UCID ].Deposit = atof( rcbankRow[0] );
+        players[ packet->UCID ].Dep_Date_create = atof( rcbankRow[1] );
+    }
+    else
+    {
+        sprintf(query,"INSERT INTO bank_deposits (username,cash,date_create) VALUES ('%s',0,0);",packet->UName);
+        if( mysql_ping( &rcbankDB ) != 0 )
+        {
+            printf("Error deposits: connection with MySQL server was lost\n");
+        }
+        if( mysql_query( &rcbankDB , query) != 0 )
+        {
+            printf("Error deposits: MySQL Query\n");
+        }
+
+        players[ packet->UCID ].Deposit = 0;
+        players[ packet->UCID ].Dep_Date_create = 0;
+    }
+    mysql_free_result( rcbankRes );
+
     /** credit **/
     if (players[ packet->UCID ].Date_create!=0)
     {
@@ -267,16 +305,12 @@ void RCBank::insim_cnl( struct IS_CNL* packet )
 
 void RCBank::bank_save (byte UCID)
 {
-
     char query[128];
     sprintf(query,"UPDATE bank SET cash = %f WHERE username='%s'" , players[ UCID ].Cash, players[ UCID ].UName);
-
     if( mysql_ping( &rcbankDB ) != 0 )
         printf("Bank Error: connection with MySQL server was lost\n");
-
     if( mysql_query( &rcbankDB , query) != 0 )
         printf("Bank Error: MySQL Query Save\n");
-
     printf("Bank Log: Affected rows = %d\n", mysql_affected_rows( &rcbankDB ) );
 
     /* Credit */
@@ -285,18 +319,22 @@ void RCBank::bank_save (byte UCID)
         printf("Credit Error: connection with MySQL server was lost\n");
     if( mysql_query( &rcbankDB , query) != 0 )
         printf("Credit Error: MySQL Query Save\n");
-
     printf("Credit Log: Affected rows = %d\n", mysql_affected_rows( &rcbankDB ) );
+
+    /* Deposit */
+    sprintf(query,"UPDATE bank_deposits SET cash = %d, date_create = %d WHERE username='%s'" , players[ UCID ].Deposit, players[ UCID ].Dep_Date_create, players[ UCID ].UName);
+    if( mysql_ping( &rcbankDB ) != 0 )
+        printf("Deposit Error: connection with MySQL server was lost\n");
+    if( mysql_query( &rcbankDB , query) != 0 )
+        printf("Deposit Error: MySQL Query Save\n");
+    printf("Deposit Log: Affected rows = %d\n", mysql_affected_rows( &rcbankDB ) );
 
     /* Capital */
     sprintf(query,"UPDATE bank SET cash = %f WHERE username='_RC_Bank_Capital_'" , BankFond);
-
     if( mysql_ping( &rcbankDB ) != 0 )
         printf("Bank Error: connection with MySQL server was lost\n");
-
     if( mysql_query( &rcbankDB , query) != 0 )
         printf("Bank Error: MySQL Query Save\n");
-
     printf("Capital Log: Affected rows = %d\n", mysql_affected_rows( &rcbankDB ) );
 
 }
@@ -315,26 +353,12 @@ void RCBank::insim_mso( struct IS_MSO* packet )
     if (players[ packet->UCID ].InZone)
     {
         char Text[96];
+
+        /** Кредиты **/
+
         //Доступная сумма кредита:
         int cr = dl->GetLVL( packet->UCID ) * 10000;
         if (cr>500000) cr = 500000;
-
-        if (strncmp(Message, "!info", strlen("!info")) == 0 )
-        {
-
-            send_mtc( packet->UCID ,"^5| ^CИнформация");
-            if (dl->GetLVL( packet->UCID )<5)
-            {
-                send_mtc( packet->UCID ,"^1| ^C^7Нужен уровень: ^15");
-                return;
-            }
-            //кредит
-            sprintf(Text,"^5| ^7^CВам доступен кредит на сумму от %d ^3RUR ^7до %d ^3RUR^7.",cr/5,cr);
-            send_mtc( packet->UCID , Text);
-            send_mtc( packet->UCID ,"^5| ^C^7Кредит выдается под ^230 %^7, на срок не более ^230^7 дней.");
-            send_mtc( packet->UCID ,"^5| ^C^7При досрочном погашении будет снята полная сумма кредита + процент.");
-            //send_mtc( packet->UCID ,"^6| ^C^7При просрочке более чем на ^21 ^7день будет снята сумма кредита + полный процент + штраф.");
-        }
 
         if (strncmp(Message, "!credit info", strlen("!credit info")) == 0 )
         {
@@ -343,7 +367,18 @@ void RCBank::insim_mso( struct IS_MSO* packet )
                 send_mtc( packet->UCID ,"^5| ^C^7Нужен уровень: ^15");
                 return;
             }
-
+            strtok (Message," ");
+            strtok (NULL," ");
+            int summ = atoi(strtok (NULL," "));
+            if (summ <= 0 )
+				summ = cr;
+            else if( summ < cr / 5 or summ > cr )
+            {
+                sprintf(Text,"^5| ^7^CОшибка. ^7Укажите сумму от %d ^3RUR ^7до %d ^3RUR^7.",cr/5,cr);
+                send_mtc( packet->UCID , Text);
+                return;
+            }
+            send_mtc( packet->UCID ,"^5| ^CИнформация по кредиту");
             if (players[ packet->UCID ].Date_create!=0)
             {
                 struct tm * timeinfo;
@@ -357,55 +392,45 @@ void RCBank::insim_mso( struct IS_MSO* packet )
                 send_mtc( packet->UCID , Text);
                 sprintf( Text,"^5| ^7^CСумма возврата: %d ^3RUR^7.",players[ packet->UCID ].Credit * 13 / 10 );
 				send_mtc( packet->UCID , Text);
-                sprintf(Text,"^5| ^7^CВремя до снятия: ^2%3.0f ^7дней.",razn);
+                sprintf(Text,"^5| ^7^CВремя до снятия: ^2%0.0f ^7дней.",razn);
                 send_mtc( packet->UCID , Text);
                 return;
             }
-
-            strtok (Message," ");
-            strtok (NULL," ");
-            int summ = atoi(strtok (NULL," "));
-            if (summ <= 0 )
-				summ = cr;
-            else if( summ < cr / 5 or summ > cr )
-            {
-                sprintf(Text,"^5| ^7^CОшибка. ^7Укажите сумму от %d ^3RUR ^7до %d ^3RUR^7.",cr/5,cr);
-                send_mtc( packet->UCID , Text);
-                return;
-            }
-
-            send_mtc( packet->UCID ,"^5| ^CИнформация по оформляемому кредиту");
+            sprintf(Text,"^5| ^7^CВам доступен кредит на сумму от %d ^3RUR ^7до %d ^3RUR^7.",cr/5,cr);
+            send_mtc( packet->UCID , Text);
+            send_mtc( packet->UCID ,"^5| ^C^7Кредит выдается под ^230 %^7, на срок не более ^230^7 дней.");
+            send_mtc( packet->UCID ,"^5| ^C^7При досрочном погашении будет снята полная сумма кредита + процент.");
             sprintf( Text,"^5| ^7^CЖелаемая сумма: %d ^3RUR^7.",summ );
             send_mtc( packet->UCID , Text);
             sprintf( Text,"^5| ^7^CСумма возврата: %d ^3RUR^7.",summ * 13 / 10 );
             send_mtc( packet->UCID , Text);
             return;
         }
-
         if (strncmp(Message, "!credit", strlen("!credit")) == 0 )
         {
+        	if (players[ packet->UCID ].Dep_Date_create!=0)
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7Вы не можете взять кредит, не закрыв вклад.");
+                return;
+            }
             if ( dl->GetLVL( packet->UCID ) < 5 )
             {
                 send_mtc( packet->UCID ,"^5| ^C^7Нужен уровень: ^15");
                 return;
             }
-
             if( players[ packet->UCID ].Date_create != 0 )
             {
                 send_mtc( packet->UCID ,"^5| ^C^7У вас уже оформлен кредит (^3!credit info^7)");
                 return;
             }
-
             strtok (Message," ");
             int summ = atoi(strtok (NULL," "));
-
             if ( summ < cr / 5 or summ > cr)
             {
                 sprintf( Text ,"^5| ^7^CОшибка. ^7Укажите сумму от %d ^3RUR ^7до %d ^3RUR^7.",cr/5,cr);
                 send_mtc( packet->UCID , Text);
                 return;
             }
-
             if (players[ packet->UCID ].Cash>(summ*3/2))
             {
                 send_mtc( packet->UCID ,"^5| ^C^7В выдаче кредита ^1отказано^7.");
@@ -417,34 +442,159 @@ void RCBank::insim_mso( struct IS_MSO* packet )
             send_mtc( packet->UCID , Text );
             players[ packet->UCID ].Credit = summ;
             players[ packet->UCID ].Date_create = time(NULL);
-
             RemFrBank(summ); //берем бабки из банка
             AddCash( packet->UCID ,summ,false); //выдаем бабки игроку
             bank_save( packet->UCID ); //сохраняемся
         }
-
         if (strncmp(Message, "!repay", strlen("!repay")) == 0 )
         {
             if ( players[ packet->UCID ].Date_create == 0 )
             {
-                send_mtc( packet->UCID ,"^5| ^C^7У вас нет кредитов");
+                send_mtc( packet->UCID ,"^5| ^C^7У вас нет кредитов.");
                 return;
             }
-
             if (players[ packet->UCID ].Cash<players[ packet->UCID ].Credit*13/10)
             {
-                send_mtc( packet->UCID ,"^5| ^C^7На вашем счете недостаточно средств для погашения кредита");
+                send_mtc( packet->UCID ,"^5| ^C^7На вашем счете недостаточно средств для погашения кредита.");
+                return;
+            }
+            send_mtc( packet->UCID , "^5| ^7^CВы погасили кредит.");
+            RemCash( packet->UCID, players[ packet->UCID ].Credit * 13 / 10 ); //отбираем бабки у игрока
+            AddToBank( players[ packet->UCID ].Credit * 13 / 10 ); //сдаем в банк
+            players[ packet->UCID ].Credit = 0;
+            players[ packet->UCID ].Date_create = 0;
+            bank_save( packet->UCID ); //сохраняемся
+        }
+
+        /** Вклады **/
+
+        //Доступная сумма вклада:
+        int dr = dl->GetLVL( packet->UCID ) * 10000;
+        if (dr>500000) dr = 500000;
+		if (strncmp(Message, "!deposit info", strlen("!deposit info")) == 0 )
+        {
+            if (dl->GetLVL( packet->UCID ) < 20 )
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7Нужен уровень: ^120");
+                return;
+            }
+            strtok (Message," ");
+            strtok (NULL," ");
+            int summ = atoi(strtok (NULL," "));
+            if (summ <= 0 )
+				summ = dr;
+            else if( summ > dr )
+            {
+                sprintf(Text,"^5| ^7^CОшибка. ^7Укажите сумму до %d ^3RUR.",dr);
+                send_mtc( packet->UCID , Text);
+                return;
+            }
+            send_mtc( packet->UCID ,"^5| ^CИнформация по вкладу");
+            if (players[ packet->UCID ].Dep_Date_create!=0)
+            {
+                struct tm * timeinfo;
+                char DateCreate [80];
+                timeinfo = localtime (&players[ packet->UCID ].Dep_Date_create);
+                strftime (DateCreate,80,"%d.%m.%Y",timeinfo);
+
+                double razn=((double)(players[ packet->UCID ].Dep_Date_create)+30*24*3600 - (double)time(NULL))/(24*3600);
+                sprintf(Text,"^5| ^7^CВы имеете вклад на сумму %d ^3RUR^7. Дата открытия: %s",players[ packet->UCID ].Deposit,DateCreate);send_mtc( packet->UCID , Text);
+                if (razn<=0)
+				{
+					sprintf( Text,"^5| ^7^CСумма на данный момент: %d ^3RUR^7 (%d ^3RUR^7 в сутки)",players[ packet->UCID ].Deposit + players[ packet->UCID ].Deposit * 5/1000 * 30, 0 ); send_mtc( packet->UCID , Text);
+					sprintf(Text,"^5| ^7^CСрок вклада истек, проценты не начисляются.");send_mtc( packet->UCID , Text);
+					return;
+				}
+                sprintf( Text,"^5| ^7^CСумма на данный момент: %d ^3RUR^7 (%d ^3RUR^7 в сутки)",players[ packet->UCID ].Deposit + players[ packet->UCID ].Deposit * 5/1000 * (int)(30-razn), players[ packet->UCID ].Deposit * 5/1000 ); send_mtc( packet->UCID , Text);
+                sprintf(Text,"^5| ^7^CДо закрытия вклада осталось: ^2%0.0f ^7дней.",razn);send_mtc( packet->UCID , Text);
                 return;
             }
 
-            send_mtc( packet->UCID , "^5| ^7^CВы погасили кредит.");
-            players[ packet->UCID ].Credit = 0;
-            players[ packet->UCID ].Date_create = 0;
-
-            RemCash( packet->UCID, players[ packet->UCID ].Credit * 13 / 10 ); //отбираем бабки у игрока
-            AddToBank( players[ packet->UCID ].Credit * 13 / 10 ); //сдаем в банк
+            sprintf(Text,"^5| ^7^CВам доступен вклад на сумму до %d ^3RUR.",dr);send_mtc( packet->UCID , Text);
+            send_mtc( packet->UCID ,"^5| ^C^7Вклад открывается под ^215 %^7 в месяц, на срок не более ^230^7 дней.");
+            sprintf( Text,"^5| ^7^CЖелаемая сумма вклада: %d ^3RUR^7.",summ );send_mtc( packet->UCID , Text);
+            sprintf( Text,"^5| ^7^CСумма с учетом процентов через ^230 ^7дней: %d ^3RUR^7 (%d ^3RUR^7 в сутки).",summ * 115 / 100,summ * 15 / 3000 );send_mtc( packet->UCID , Text);
+            sprintf( Text,"^5| ^7^CВы можете закрыть вклад не ранее чем через ^214 ^7дней с момента вклада.",summ );send_mtc( packet->UCID , Text);
+            return;
+        }
+        if (strncmp(Message, "!deposit", strlen("!deposit")) == 0 )
+        {
+        	if (players[ packet->UCID ].Date_create!=0)
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7Вы не можете открыть вклад, не погасив кредит.");
+                return;
+            }
+            if ( dl->GetLVL( packet->UCID ) < 5 )
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7Нужен уровень: ^120");
+                return;
+            }
+            if( players[ packet->UCID ].Dep_Date_create != 0 )
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7У вас уже открыт вклад (^3!deposit info^7).");
+                return;
+            }
+            strtok (Message," ");
+            int summ = atoi(strtok (NULL," "));
+            if (summ > dr or summ==0)
+            {
+                sprintf(Text,"^5| ^7^CОшибка. ^7Укажите сумму до %d ^3RUR.",dr); send_mtc( packet->UCID , Text);
+                return;
+            }
+            if (players[ packet->UCID ].Cash<=(summ))
+            {
+                send_mtc( packet->UCID ,"^5| ^C^7На вашем счете недостаточно средств для открытия вклада.");
+                return;
+            }
+            //создаем вклад
+            sprintf(Text,"^5| ^7^CВы открыли вклад на сумму %d ^3RUR^7.", summ ); send_mtc( packet->UCID , Text );
+            players[ packet->UCID ].Deposit = summ;
+            players[ packet->UCID ].Dep_Date_create = time(NULL);
+            RemCash( packet->UCID ,summ); //снимаем у игрока
+            AddToBank(summ); //кладем в банк
             bank_save( packet->UCID ); //сохраняемся
         }
+        if (strncmp(Message, "!withdraw", strlen("!withdraw")) == 0 )
+        {
+            strtok (Message," ");
+        	double razn=((double)(players[ packet->UCID ].Dep_Date_create)+30*24*3600 - (double)time(NULL))/(24*3600);
+            int dtt;
+			char * s;
+			s = strtok(NULL," ");
+			if (s != NULL)
+			{
+				if (strncmp(s, "yes", strlen("yes")) == 0)
+				{
+					sprintf(Text,"^5| ^7^CВы закрыли вклад."); send_mtc( packet->UCID , Text );
+					dtt = players[ packet->UCID ].Deposit + players[ packet->UCID ].Deposit * 5/1000 * (int)(30-razn);
+					sprintf(Text,"^5| ^7^CБанк получает неустойку в размере %d ^3RUR^7.",dtt*5/100); send_mtc( packet->UCID , Text );
+					dtt = dtt - dtt*5/100;
+					RemFrBank(dtt); //берем из банка
+					AddCash( packet->UCID ,dtt, true); //отдаем игроку
+					players[ packet->UCID ].Deposit = 0;
+					players[ packet->UCID ].Dep_Date_create = 0;
+					bank_save( packet->UCID ); //сохраняемся
+					return;
+				}
+			}
+            if (razn>16)
+			{
+				sprintf(Text,"^5| ^C^1Внимание! ^7Еще не прошло 14 дней с момента вклада."); send_mtc( packet->UCID , Text );
+				send_mtc( packet->UCID ,"^5| ^7^CВведите ^3!withdraw yes ^7чтобы закрыть вклад. Банк получит неустойку в размере ^25 %^7.");
+				return;
+			}
+        	sprintf(Text,"^5| ^7^CВы закрыли вклад."); send_mtc( packet->UCID , Text );
+			if (razn<=0)
+				dtt = players[ packet->UCID ].Deposit + players[ packet->UCID ].Deposit * 5/1000 * 30;
+			else
+				dtt = players[ packet->UCID ].Deposit + players[ packet->UCID ].Deposit * 5/1000 * (int)(30-razn);
+			RemFrBank(dtt); //берем из банка
+			AddCash( packet->UCID ,dtt, true); //отдаем игроку
+			players[ packet->UCID ].Deposit = 0;
+			players[ packet->UCID ].Dep_Date_create = 0;
+			bank_save( packet->UCID ); //сохраняемся
+        }
+
     }
 }
 
@@ -466,10 +616,12 @@ void RCBank::insim_mci( struct IS_MCI* pack_mci )
 				players[ UCID ].InZone=true;
 
 				send_mtc( UCID ,"^5| Bank");
-				send_mtc( UCID ,"^5| ^7!info - ^2^CОбщая информация");
-				send_mtc( UCID ,"^5| ^7!credit info N - ^2^CРасчет процентов по кредиту на сумму N");
+				send_mtc( UCID ,"^5| ^7!credit info N - ^2^CИнформация по кредиту на сумму N");
 				send_mtc( UCID ,"^5| ^7!credit N (!credit 50000) - ^2^CПолучить кредит на сумму N");
 				send_mtc( UCID ,"^5| ^7!repay - ^2^CПогасить кредит");
+				send_mtc( UCID ,"^5| ^7!deposit info N - ^2^CИнформация по вкладу на сумму N");
+				send_mtc( UCID ,"^5| ^7!deposit N (!deposit 200000)- ^2^CОткрыть вклад на сумму N");
+				send_mtc( UCID ,"^5| ^7!withdraw - ^2^CЗакрыть вклад");
 			}
 		}
 		else if(players[ UCID ].InZone)
