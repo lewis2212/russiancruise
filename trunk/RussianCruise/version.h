e851d8ddd9aa88d152f3d15cf30d581021a7ec24
turbosnail.rus@gmail.com <turbosnail.rus@gmail.com@e20eac7a-df01-d0be-1f0a-6011d65844bd>
2013-05-19 15:06:37 +0000

    - мелкие правки перед релизом Знаков

--- trunk/RussianCruise/version.h
+++ trunk/RussianCruise/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char RC_DATE[] = "16";
-	static const char RC_MONTH[] = "04";
+	static const char RC_DATE[] = "19";
+	static const char RC_MONTH[] = "05";
 	static const char RC_YEAR[] = "2013";
-	static const char RC_UBUNTU_VERSION_STYLE[] = "13.04";
+	static const char RC_UBUNTU_VERSION_STYLE[] = "13.05";
 	
 	//Software Status
 	static const char RC_STATUS[] = "Beta";
@@ -16,18 +16,18 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long RC_MAJOR = 1;
 	static const long RC_MINOR = 2;
-	static const long RC_BUILD = 19;
+	static const long RC_BUILD = 20;
 	static const long RC_REVISION = 531;
 	
 	//Miscellaneous Version Types
-	static const long RC_BUILDS_COUNT = 707;
-	#define RC_RC_FILEVERSION 1,2,19,531
-	#define RC_RC_FILEVERSION_STRING "1, 2, 19, 531\0"
-	static const char RC_FULLVERSION_STRING[] = "1.2.19.531";
+	static const long RC_BUILDS_COUNT = 709;
+	#define RC_RC_FILEVERSION 1,2,20,531
+	#define RC_RC_FILEVERSION_STRING "1, 2, 20, 531\0"
+	static const char RC_FULLVERSION_STRING[] = "1.2.20.531";
 	
 	//SVN Version
-	static const char RC_SVN_REVISION[] = "141";
-	static const char RC_SVN_DATE[] = "2013-04-16T04:51:24.456162Z";
+	static const char RC_SVN_REVISION[] = "0";
+	static const char RC_SVN_DATE[] = "unknown date";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long RC_BUILD_HISTORY = 1;
