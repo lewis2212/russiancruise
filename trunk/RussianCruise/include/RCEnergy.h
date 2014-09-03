3facad56a0f1543342d6d99e097a100fa250583f
turbosnail.rus <turbosnail.rus@e20eac7a-df01-d0be-1f0a-6011d65844bd>
2014-09-03 12:19:05 +0000

    new: Плавное заполнение энергией

--- trunk/RussianCruise/include/RCEnergy.h
+++ trunk/RussianCruise/include/RCEnergy.h
@@ -14,6 +14,7 @@ struct EnergyPlayer: public GlobalPlayer
 
     /** Energy **/
     int     Energy; // 0-10000
+    int     leftToFeel = 0;
     time_t  EnergyTime;
     bool	EnergyAlarm = false;
     bool	Lock;
